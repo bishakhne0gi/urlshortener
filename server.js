b192852e9e5937d77c554fdf74d778e4979e3b91
Bishakh Neogi <50258639+bishakhne0gi@users.noreply.github.com>
2022-03-28 10:33:42 +0530

    initial commit

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const ShortUrl = require('./models/shortUrl')
 const dotEnv = require('dotenv')
 const app = express()
 
-// dotEnv.config()
+dotEnv.config()
 
-mongoose.connect("mongodb+srv://root:znlujL7nOwm2kuM4@cluster0.hlkwr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+mongoose.connect(process.env.MongoUrl, {
   useNewUrlParser: true
 }).then(()=>{
     console.log("connected to database")
